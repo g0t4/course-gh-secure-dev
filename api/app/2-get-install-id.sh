f0cc6ee5a703ded7ce19bda062ab603276de0614
Wes Higbee <wes.mcclure@gmail.com>
2024-12-17 22:51:42 -0600

    rearrange

---
+++ api/app/2-get-install-id.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Get Install ID
+curl -sSL --request GET \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: Bearer $(cat current.jwt)" \
+    --header "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/app/installations |
+    jq ".[] | {id, app_slug}"
+# review app installs
+# save install_id to file:
+
+## other things to try:
+#
+#     https://api.github.com/app
+#
+# # (optional) HEADERS for rate limit info
+# curl -sSL -I --request GET \
+#     --header "Accept: application/vnd.github+json" \
+#     --header "Authorization: Bearer $(cat current.jwt)" \
+#     --header "X-GitHub-Api-Version: 2022-11-28" \
+#     https://api.github.com/app/installations
--- api/app/3-install-token.sh
+++ api/app/3-install-token.sh
@@ -1,35 +1,8 @@
 #!/usr/bin/env bash
 
-# (optional) Get App Info (with JWT alone)
-curl -sSL --request GET \
-    --header "Accept: application/vnd.github+json" \
-    --header "Authorization: Bearer $(cat current.jwt)" \
-    --header "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/app
 
 
-
-# Get Install ID
-curl -sSL --request GET \
-    --header "Accept: application/vnd.github+json" \
-    --header "Authorization: Bearer $(cat current.jwt)" \
-    --header "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/app/installations \
-    | jq ".[] | {id, app_slug}"
-# review app installs
-install_id=XXXXXX # TODO paste install id here
-
-
-
-# (optional) HEADERS for rate limit info
-curl -sSL -I --request GET \
-    --header "Accept: application/vnd.github+json" \
-    --header "Authorization: Bearer $(cat current.jwt)" \
-    --header "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/app/installations
-
-
-
+install_id=$(cat install_id.txt)
 
 # Get Installation Access Token
 # https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-an-installation-access-token-for-a-github-app
